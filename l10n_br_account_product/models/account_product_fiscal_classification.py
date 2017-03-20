9487006c61ebb3a06ed0d3969dd176b116547e43
pcastilho <paulo_henryc@outlook.com>
2017-03-20 08:41:24 -0300

    [DEV] SOLUCAO CFOP MULTIPLOS
    SOLUCAO CFOP MULTIPLOS

--- l10n_br_account_product/models/account_product_fiscal_classification.py
+++ l10n_br_account_product/models/account_product_fiscal_classification.py
@@ -91,12 +91,12 @@ class L10nBrTaxDefinitionTemplateModel(L10nBrTaxDefinitionTemplate):
     tax_icms_relief_id = fields.Many2one(
         'l10n_br_account_product.icms_relief', string=u'Desoneração ICMS')
 		
-	#<BNZ - SOLUCAO CFOP MULTIPLOS>
-	cfop_estado_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Estado')
-	cfop_outros_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Outros')
-	cfop_exterior_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Exterior')
-	cfop_entrada_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Entrada')
-	#<\BNZ>
+#<BNZ - SOLUCAO CFOP MULTIPLOS>
+cfop_estado_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Estado')
+cfop_outros_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Outros')
+cfop_exterior_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Exterior')
+cfop_entrada_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Entrada')
+#<\BNZ>
 
     _sql_constraints = [
         ('l10n_br_tax_definition_template_tax_template_id_uniq', 'unique \
@@ -339,12 +339,12 @@ class L10nBrTaxDefinitionModel(L10nBrTaxDefinition):
     tax_icms_relief_id = fields.Many2one(
         'l10n_br_account_product.icms_relief', string=u'Desoneração ICMS')
 		
-	#<BNZ - SOLUCAO CFOP MULTIPLOS>
-	cfop_estado_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Estado')
-	cfop_outros_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Outros')
-	cfop_exterior_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Exterior')
-	cfop_entrada_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Entrada')
-	#<\BNZ>
+#<BNZ - SOLUCAO CFOP MULTIPLOS>
+cfop_estado_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Estado')
+cfop_outros_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Outros')
+cfop_exterior_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Exterior')
+cfop_entrada_id = fields.Many2one('l10n_br_account_product.cfop', 'CFOP Entrada')
+#<\BNZ>
 
     _sql_constraints = [
         ('l10n_br_tax_definition_tax_id_uniq', 'unique (tax_id,\
